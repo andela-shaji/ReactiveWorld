54ac157e6cb44bb8306eda9a61858ed057b9b5bd
“Suada <suada.haji@andela.com>
2017-01-05 21:23:11 +0300

    Initial commit

---
+++ app/src/main/java/com/android/java/miss/helloreactiveworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.java.miss.helloreactiveworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
